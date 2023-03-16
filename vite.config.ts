80b16485fa746c4f61c5286ab20de2b361700f57
ccitsxy <1127885451@qq.com>
2023-03-16 19:05:20 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 import vueMacros from 'unplugin-vue-macros/vite';
+import inspect from 'vite-plugin-inspect';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
         vue: vue(),
       },
     }),
+    inspect(),
   ],
 });
